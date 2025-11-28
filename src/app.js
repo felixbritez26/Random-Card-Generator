7609bc652d7985508bfa6fd99a490590af612f3a
felixbritez26 <mauri.britez2017@hotmail.com>
2025-11-28 02:12:49 +0000

    Randomcardgenerator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,23 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+const suits = ["♥", "♦", "♠", "♣"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+function getRandomItem(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+function generateRandomCard(){
+  const suit = getRandomItem(suits);
+  const value = getRandomItem(values);
+  const card = document.querySelector(".card");
+  const top = document.querySelector(".suit.top");
+  const bottom = document.querySelector(".suit.bottom");
+  const center = document.querySelector(".value");
+  top.textContent = suit;
+  bottom.textContent = suit;
+  center.textContent = value;
+  card.classList.remove("red");
+}
+window.onload = generateRandomCard;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
